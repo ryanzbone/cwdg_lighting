1dbbfbe12edfc5fc80925de4205547da06c046b1
Ryan Bone <ryanzbone@gmail.com>
2014-01-16 20:40:07 -0500

    add mode switching between edit and presentation

--- cwdg_lightning.js
+++ cwdg_lightning.js
@@ -1,15 +1,70 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to cwdg_lightning.";
-  };
+Slides = new Meteor.Collection("slides");
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+if (Meteor.isClient) {
+
+  Template.controls.events({
+    'click a#change-modes' : function() {
+      $('#the-editor').toggle();
+      $('#presentation').toggle();
+      if ($('#change-modes').text() === 'Editor') {
+        $('#change-modes').text('Presentation');
+      } else {
+        $('#change-modes').text('Editor');
+      }
     }
   });
+
+  Template.presentation.slides = function() {
+    return Slides.find({}, {sort: { number: 1 } }).fetch();
+  };
+
+  Template.presentation.events({
+    'click a#next' : function() {
+
+    }
+  });
+
+  Template.new_slide.events({
+    'click a#new-slide' : function() {
+      return Template.edit();
+    }
+  });
+
+  Template.slide_display.slides = function() {
+    return Slides.find({}, {sort: { number: 1 } }).fetch();
+  };
+
+  Template.slide_display.events({
+    'click a.edit-link' : function() {
+      var id = this._id;
+      $('.md-display-' + id).toggle();
+      $('.edit-' + id).toggle();
+      $('.edit-' + id).find('.title-' + id).val(this.title);
+      $('.edit-' + id).find('.number-' + id).val(this.number);
+      $('.edit-' + id).find('.content-' + id).val(this.content);
+    },
+
+    'click a.delete-link' : function() {
+      Slides.remove(this._id);
+    },
+
+    'click .save' : function() {
+      var id = this._id;
+      Slides.update(this._id, {title: $('.title-' + id).val(), content: $('.content-' + id).val(), number: parseInt($('.number-' + id).val())});
+      $('.md-display-' + id).toggle();
+      $('.edit-' + id).toggle();
+    }
+  });
+
+  Template.slide_info.events({
+    'click .submit' : function() {
+      Slides.insert({title: $('#title').val(), content: $('#content').val(), number: parseInt($('#number').val())});
+      $('#title').val('');
+      $('#number').val('');
+      $('#content').val('');
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
